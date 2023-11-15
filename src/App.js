b989f387c6b2c187b52f7c676d42c784fb12fd7b
arun <arunrajrmvnr@gmail.com>
2023-11-15 09:38:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Pass from "./components/password";
+function App() {
+  return (
+    <>
+      <Pass />
+    </>
+  );
+}
+
+export default App;
